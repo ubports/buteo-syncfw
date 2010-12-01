3e4c941b42ec353d33809de197ed13c7c9e3a065
Duggirala Karthik <karthik.2.duggirala@nokia.com>
2010-12-01 14:28:50 +0200

    Minor change in test script

--- unittests/tests/runstarget.sh
+++ unittests/tests/runstarget.sh
@@ -7,7 +7,7 @@ FILE=${1##*/}
 cd /usr/share/sync-fw-tests
 export LD_LIBRARY_PATH=/usr/share/sync-fw-tests:$LD_LIBRARY_PATH
 
-meego-run ${1} -maxwarnings 0 1>/tmp/$FILE.out 2>&1
+${1} -maxwarnings 0 1>/tmp/$FILE.out 2>&1
 RESULT=$?
 
 echo "$RESULT is return value of executing ${1}" >> /tmp/$FILE.out
