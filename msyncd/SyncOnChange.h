7643bf1c9750f88cbfb8aba9cc11312f5b72ea9c
Deepak Kodihalli <deepak.kodihalli@nokia.com>
2011-01-24 11:11:49 +0530

    Initial changes for sync on change feature: * StorageChangeNotifierPlugin.h - API for storage change notifier plug-ins * StorageChangeNotifier - messenger between storage change notifier plug-ins and SyncOnChange * SyncOnChange - monitors storages for changes and initiates sync on change for interested sync profiles.

---
+++ msyncd/SyncOnChange.h
@@ -0,0 +1,73 @@
+#ifndef SYNCONCHANGE_H
+#define SYNCONCHANGE_H
+
+#include <QObject>
+#include <QHash>
+
+namespace Buteo
+{
+
+class SyncProfile;
+class StorageChangeNotifier;
+class PluginManager;
+
+/*! \brief this class initiates a sync if there are changes
+ * in storage(s) it's asked to monitor
+ */
+class SyncOnChange : public QObject
+{
+    Q_OBJECT
+
+public:
+    /*! \brief constructor
+     */
+    SyncOnChange();
+
+    /*! \brief destructor
+     */
+    ~SyncOnChange();
+
+    /*! \brief enable sync on change for a list of storages
+     * for the interested profiles
+     *
+     * Destroys the profile objects when they are no longer needed
+     *
+     * @param aPluginManager Used to load SOC storage plug-ins
+     * @param aSOCStorageMap map of well-known storage name
+     * to list of sync profiles insterested in SOC for that
+     * storage
+     * @param list of storage names for which SOC couldn't be enabled
+     * @return false if SOC can't be enabled for one or more
+     * storages
+     */
+    bool enable(const QHash<QString,QList<SyncProfile*> >& aSOCStorageMap,
+                PluginManager* aPluginManager, QStringList& aFailedStorages);
+
+    /*! \brief disable sync on change
+     */
+    void disable();
+
+public Q_SLOTS:
+    /*! initiate sync for this storage
+     */
+    void sync(QString aStorageName);
+
+private:
+    /*! \brief destroys profile objects interested in SOC for this
+     * storage
+     */
+    void cleanup(const QString& aStorageName);
+
+    /*! \brief Get the names for storages for which SOC is desired
+     *
+     * @return list of storage names
+     */
+    QStringList getSOCStorageNames();
+
+    StorageChangeNotifier* iStorageChangeNotifier;
+    QHash<QString,QList<SyncProfile*> > iSOCStorageMap;
+};
+
+}
+
+#endif
