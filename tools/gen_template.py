f3d7f2aa520b1406b5a5ce675874f904bb3b875a
Sateesh Kavuri <sateesh.kavuri@gmail.com>
2013-07-30 17:39:46 +0530

    Added support for generating sync profile xml files
    With this patch sync profile xml files can be generated. Next step is to
    generate the storage profile as well as the agent profiles
    
    Signed-off-by: Sateesh Kavuri <sateesh.kavuri@gmail.com>

--- tools/gen_template.py
+++ tools/gen_template.py
@@ -42,17 +42,18 @@ def main():
     # Validate the config file
     validator = ConfigValidator (cf)
     validator.validate ()
-    
+
     tg = TemplateGenerator (cf, "test")
     tg.createDirStructure()
     tg.generateClasses()
+    tg.generateProfiles()
 
 '''
     Class to validate the configuration input
 '''
 class ConfigValidator:
     def __init__(self, configParser):
-        self.mandatory_client_config_keys  = set(['classname', 'name', 'destinationtype', 'schedule', 'displayname', 'transport', 'enabled'])
+        self.mandatory_client_config_keys  = set(['classname', 'name', 'destinationtype', 'schedule', 'displayname', 'transport', 'enabled', 'agent_name', 'syncdirection', 'conflictpolicy'])
         self.mandatory_server_config_keys  = set(['classname', 'name', 'transport', 'enabled'])
         self.mandatory_storage_config_keys = set(['classname', 'name', 'mimetype', 'enabled'])
         self.type = ''
@@ -73,7 +74,7 @@ class ConfigValidator:
             self.suppliedtype = configParser.sections ()[0]
             print "Error: Invalid plug-in type '" + self.suppliedtype + "' specified. Should be one of 'clientconfig/serverconfig/storageconfig'"
             sys.exit (3)
-        
+
         self.values = dict (configParser.items (self.type))
 
     def validate (self):
@@ -101,20 +102,23 @@ class TemplateGenerator:
     def __init__ (self, configParser, targetDir):
         self.targetDir = targetDir
         self.configParser = configParser
-        
+
         self.type = ''
         if self.configParser.has_section ('clientconfig'):
             self.type = 'clientconfig'
-            self.classtemplate_h = 'clientplugin.h.tmpl'
-            self.classtemplate_cpp = 'clientplugin.cpp.tmpl'
+            self.classtemplate_h = 'clientplugin_h.tmpl'
+            self.classtemplate_cpp = 'clientplugin_cpp.tmpl'
+            self.profile = 'syncprofile_xml.tmpl'
         elif self.configParser.has_section ('serverconfig'):
             self.type = 'serverconfig'
-            self.classtemplate_h = 'serverplugin.h.tmpl'
-            self.classtemplate_cpp = 'serverplugin.cpp.tmpl'
+            self.classtemplate_h = 'serverplugin_h.tmpl'
+            self.classtemplate_cpp = 'serverplugin_cpp.tmpl'
+            self.profile = 'serverprofile_xml.tmpl'
         elif self.configParser.has_section ('storageconfig'):
             self.type = 'storageconfig'
-            self.classtemplate_h = 'storageplugin.h.tmpl'
-            self.classtemplate_cpp = 'storageplugin.cpp.tmpl'
+            self.classtemplate_h = 'storageplugin_h.tmpl'
+            self.classtemplate_cpp = 'storageplugin_cpp.tmpl'
+            self.profile = 'storageprofile_xml.tmpl'
 
     def createDirStructure (self):
         try:
@@ -123,7 +127,7 @@ class TemplateGenerator:
         except OSError as exception:
             if exception.errorno != errno.EEXIST:
                 raise
-        
+
     def cleanup (self):
         inp = 'yn'
         count = 1
@@ -143,12 +147,36 @@ class TemplateGenerator:
         header = open (self.targetDir + "/" + classname + ".h", "w")
         print >> header, Template (file = self.classtemplate_h, searchList = [{'plugin':dict(self.configParser.items(self.type))}])
         header.close()
-        
+
         # Generate .cpp
         cpp = open (self.targetDir + "/" + classname + ".cpp", "w")
         print >> cpp, Template (file = self.classtemplate_cpp, searchList = [{'plugin':dict(self.configParser.items(self.type))}])
         cpp.close()
+    
+    def generateProfiles (self):
+        searchList = dict ()
+        searchList['profile'] = dict (self.configParser.items (self.type))
+        searchList['storage'] = dict (self.configParser.items ('storages'))
+        searchList['extconfig'] = dict (self.configParser.items ('ext-config'))
+
+        # Create the dir's for the xml files
+        if len (searchList) != 0:
+            os.makedirs(self.targetDir + "/xml/sync")
+            os.makedirs(self.targetDir + "/xml/client")
+        if searchList.has_key('storage'):
+            os.makedirs(self.targetDir + "/xml/storage")
         
+        profile_name = self.configParser.get (self.type, 'name')
+        agent_name = self.configParser.get (self.type, 'agent_name')
+        #storage_name = self.configParser.get ('storages', 'TBD')
+        sync_profile = open (self.targetDir + "/xml/sync/" + profile_name + ".xml", "w")
+        #client_profile = open (self.targetDir + "/xml/client/" + agent_name + ".xml", "w")
+        #storage_profile = open (self.targetDir + "/xml/storage/" + )
+
+        print >> sync_profile, Template (file = self.profile, searchList = [searchList])
+        #print >> client_profile, Template (file = )
+        sync_profile.close()
+        #client_profile.close()
 
 # End TemplateGenerator class
 
@@ -158,8 +186,6 @@ if __name__ == "__main__":
 
 #print Template (file = 'clientprofile.xml.tmpl', searchList = [{'profile':dict(cf.items('profile'))}])
 #print Template (file = 'mypluginprofile.xml.tmpl', searchList = [{'plugin':dict(cf.items('clientplugin')), 'profile':dict(cf.items('profile'))}])
-'''
-print Template (file = 'mypluginprofile.xml.tmpl', searchList = [{'plugin':clientplugin}, {'profile':clientprofile}])
-print Template (file = 'sample.tmpl', searchList = [{'plugin':clientplugin}, {'profile':clientprofile}])
-'''
+#print Template (file = 'mypluginprofile.xml.tmpl', searchList = [{'plugin':clientplugin}, {'profile':clientprofile}])
+#print Template (file = 'sample.tmpl', searchList = [{'plugin':clientplugin}, {'profile':clientprofile}])
 
